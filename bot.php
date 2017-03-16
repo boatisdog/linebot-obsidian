d728d29881850ce5e1392d4fd54ce31030170337
กิตติ์ธเนศ เหลืองสนธยา < >
2017-03-17 04:03:06 +0700

    update line bot

--- bot.php
+++ bot.php
@@ -4,6 +4,27 @@ $access_token = 'SiZyVVTPIPP4Qn9VwKKKCI0YA3yjbfpk/mjb4Az4bbnrd275417q/2+JV0XGZca
 $db = pg_connect ("postgres://krdookwgbudwkq:337d29bb2b87f471b47f286fcb7fa1fb885b4b063f9ea5197805f4f679e7d9b8@ec2-54-221-255-153.compute-1.amazonaws.com:5432/dd6j72nr8uanuq");
 //
 
+$messages = [
+	'type' => 'text',
+	'text' => "HELLO"
+];
+$data = [
+	'to' => ["Uffb752fc81a0f82fe74a413b16913d7b"],
+	'messages' => [$messages]
+];
+$url = 'https://api.line.me/v2/bot/message/push';
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $result . "\r\n";
+
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
@@ -21,7 +42,7 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 			$messages = [
 				'type' => 'text',
-				'text' => $event['source']['userId']
+				'text' => $replyToken
 			];
 			$data = [
 				'replyToken' => $replyToken,
