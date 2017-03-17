c8715335032404f2a6bc2e00780b3cf1bb3b9842
กิตติ์ธเนศ เหลืองสนธยา < >
2017-03-17 12:15:51 +0700

    updated line bot

--- bot.php
+++ bot.php
@@ -4,39 +4,27 @@
 $access_token = 'SiZyVVTPIPP4Qn9VwKKKCI0YA3yjbfpk/mjb4Az4bbnrd275417q/2+JV0XGZca29KQ1F0S1Gh4Tx3DC8mLjQYGnbVLsJzmI2AA7kRlq+983S/bm6h0u4bsEu4Iyb6sl2E8PQnm7d0wguJ3kz6pEhwdB04t89/1O/w1cDnyilFU=';
 //////////////////////
 
-// $db = pg_connect("postgres://krdookwgbudwkq:337d29bb2b87f471b47f286fcb7fa1fb885b4b063f9ea5197805f4f679e7d9b8@ec2-54-221-255-153.compute-1.amazonaws.com:5432/dd6j72nr8uanuq");
-// $query = "SELECT * FROM WEATHER_HUMIDITY WHERE hum <= 300 ORDER BY pic DESC LIMIT 1"; 
-// $result = pg_query($query); 
-// if (!$result) { 
-	// echo "Problem with query " . $query . "<br/>"; 
-	// echo pg_last_error(); 
-	// exit(); 
-// } 
-// if(pg_num_rows($result) > 0){
-	// $messages = [
-		// 'type' => 'text',
-		// 'text' => "ALERT"
-	// ];
-	// $data = [
-		// "to" => "Uffb752fc81a0f82fe74a413b16913d7b",
-		// 'messages' => [$messages]
-	// ];
-	// $url = 'https://api.line.me/v2/bot/message/push';
-// }
-// echo pg_num_rows($result);
-// echo "send";
-// $post = json_encode($data);
-// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-// $ch = curl_init($url);
-// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-// $result = curl_exec($ch);
-// curl_close($ch);
-echo $access_token . "\r\n";
-/* // Get POST body content
+$db = pg_connect("postgres://krdookwgbudwkq:337d29bb2b87f471b47f286fcb7fa1fb885b4b063f9ea5197805f4f679e7d9b8@ec2-54-221-255-153.compute-1.amazonaws.com:5432/dd6j72nr8uanuq");
+$query = "SELECT * FROM WEATHER_HUMIDITY WHERE hum <= 300 ORDER BY pic DESC LIMIT 1"; 
+$result = pg_query($query); 
+if (!$result) { 
+	echo "Problem with query " . $query . "<br/>"; 
+	echo pg_last_error(); 
+	exit(); 
+} 
+if(pg_num_rows($result) > 0){
+	$messages = [
+		'type' => 'text',
+		'text' => "ALERT"
+	];
+	$data = [
+		"to" => "Uffb752fc81a0f82fe74a413b16913d7b",
+		'messages' => [$messages]
+	];
+	$url = 'https://api.line.me/v2/bot/message/push';
+}
+
+// Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
@@ -72,7 +60,6 @@ if (!is_null($events['events'])) {
 					$output = "Weather on : ".$myrow['date_c']."\nTemp is : ".$myrow['temp']."\nWeather is : ".$myrow['weather']."\nPressure is : ".$myrow['air_p']."\nHumidity is : ".$myrow['hum'];
 					$imagename = $myrow['pic'];
 				} 
-				pg_close();
 				//////////
 				// Build message to reply back
 				$messages = [
@@ -101,7 +88,7 @@ if (!is_null($events['events'])) {
 				while($myrow = pg_fetch_assoc($result)) { 
 					$output = $output."Weather on : ".$myrow['date_c']."\nTemp is : ".$myrow['temp']."\nWeather is : ".$myrow['weather']."\nPressure is : ".$myrow['air_p']."\nHumidity is : ".$myrow['hum']."\n============================";
 				} 
-				pg_close();
+				
 				//////////
 				// Build message to reply back
 				$messages = [
@@ -140,6 +127,18 @@ if (!is_null($events['events'])) {
 			// echo $result . "\r\n";
 		}
 	}
-} */
+}
+$post = json_encode($data);
+$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+$ch = curl_init($url);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+$result = curl_exec($ch);
+curl_close($ch);
+echo $access_token . "\r\n";
+pg_close();
 //CREATE TABLE WEATHER_HUMIDITY (date_c VARCHAR(40), temp real, weather VARCHAR(30), air_p INT, hum INT, pic VARCHAR(20));
 echo "OK";
