733b56f15c15c367b36b9f637b8426259d3f3bb4
กิตติ์ธเนศ เหลืองสนธยา < >
2017-03-17 11:48:30 +0700

    updated line bot

--- bot.php
+++ bot.php
@@ -107,7 +107,7 @@ if (!is_null($events['events'])) {
 				// Build message to reply back
 				$messages = [
 					'type' => 'text',
-					'text' => json_encode($event);
+					'text' => "ssss";
 				];
 				// Make a POST Request to Messaging API to reply to sender
 				$data = [
